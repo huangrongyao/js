9eb818a6532790b084e800a078eb9c9385f7f182
huangrongyao <huangrongyao@foreverider.com>
2019-06-28 17:54:26 +0800

    1

--- deepCode.js
+++
@@ -1,3 +0,0 @@
-function deepCode(){
-    return 0
-}
---
+++ deepCopy.js
@@ -0,0 +1,17 @@
+/**
+ * 深拷贝与浅拷贝
+ * 深拷贝：
+ * 浅拷贝：
+ */
+
+// 1.es6解构
+// var a = [1,2,3]
+// var b = [...a]
+// console.log(b)
+// b.push(1)
+// console.log(b)
+// console.log(a)
+
+// 2.数组的concat与slice方法
+
+// 3.JSON对象的parse和stringify    

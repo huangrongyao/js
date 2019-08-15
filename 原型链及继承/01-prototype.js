825d61737d1f56ad096e7987f9ee37a52de6ceec
huangrongyao <huangrongyao@foreverider.com>
2019-08-15 17:47:19 +0800

    继承

---
+++ 原型链及继承/01-prototype.js
@@ -0,0 +1,18 @@
+var url = serverUrlBase + "/server/images/" + mapid + "/files/png";
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+xhr.responseType = "blob";
+xhr.setRequestHeader("client_type", "DESKTOP_WEB");
+xhr.setRequestHeader("desktop_web_access_key", _desktop_web_access_key);
+xhr.onload = function() {
+  if (this.status == 200) {
+    var blob = this.response;
+    var img = document.createElement("img");
+    img.onload = function(e) {
+      window.URL.revokeObjectURL(img.src); 
+    };
+    img.src = window.URL.createObjectURL(blob);
+    $("#imgcontainer").html(img); 
+  }
+}
+xhr.send();
